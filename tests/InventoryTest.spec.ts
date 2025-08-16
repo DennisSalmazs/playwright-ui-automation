5238d4610febd6796b26467ec980d6d8a50c28bd
DennisSalmazs <skrn.sdet@gmail.com>
2025-08-16 15:30:58 +0100

    add logs for login test

---
+++ tests/InventoryTest.spec.ts
@@ -0,0 +1,4 @@
+import { test } from "../fixtures/CustomFixtures";
+import { POManager } from "../pages/POManager";
+import { expect } from "@playwright/test";
+import { testBase } from "../fixtures/TestBase";
--- tests/LoggingTest.spec.ts
+++ tests/LoggingTest.spec.ts
@@ -1,17 +1,22 @@
 import { test } from "../fixtures/CustomFixtures";
-import { POManager } from "../pages/POManager";
 import { expect } from "@playwright/test";
+import { POManager } from "../pages/POManager";
 import { testBase } from "../fixtures/TestBase";
 
-test('User logs in', async ({ page, poManager, testData, logger, inventoryPage }) => {
-    try {
-        const { username } = await poManager.getLoginPage().login();
-        logger.pass(`User '${username}' logged in successfully`);
-      } catch (error) {
-        logger.fail("User login failed");
-        throw error;
-      }
-      await page.waitForLoadState('domcontentloaded');
-      await expect(inventoryPage.products).toBeVisible();
-
+test('User logs in successfuly', async ({ page, poManager, testData, logger, inventoryPage }) => {
+  try {
+    const { username } = await poManager.getLoginPage().login();
+    logger.pass(`User '${username}' logged in successfully`);
+  } catch (error) {
+    logger.fail("User login failed");
+    throw error;
+  }
+  await page.waitForLoadState('domcontentloaded');
+  try {
+    await expect(inventoryPage.products).toBeVisible();
+    logger.pass("Products are visible")
+  } catch (error) {
+    logger.fail("Products are not visible")
+  }
 });
+
